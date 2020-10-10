7cb83902a8f5036057e0752810cad2b82b554cf7
Nishchal Arora <narora88@gmail.com>
2020-10-10 13:50:49 +1000

    Visualization source code

---
+++ myBusinessVis.js
@@ -0,0 +1,45 @@
+function visualizeBusinesses() {
+    $.ajax({
+        url: "/mybusinessservice/businesses",
+        type: 'GET',
+        dataType: 'json',
+        success: $.proxy(function(businesses) {
+            var iconFeatures = [];
+
+            var iconStyle = new ol.style.Style({
+                image: new ol.style.Icon({
+                    src: './data/icon.png',
+                    scale: 0.1
+                })
+            });
+
+            $.each(businesses, function(index, business) {
+                var iconFeature = new ol.Feature({
+                    geometry: new ol.geom.Point(ol.proj.transform([business.lon, business.lat], 'EPSG:4326', 'EPSG:3857')),
+                    name: business.name
+                });
+                iconFeature.setStyle(iconStyle);
+                iconFeatures.push(iconFeature);
+            });
+
+            var vectorSource = new ol.source.Vector({
+                features: iconFeatures
+            });
+
+            var vectorLayer = new ol.layer.Vector({
+                source: vectorSource
+            });
+
+            this.map.addLayer(vectorLayer);
+        }, this)
+    });
+}
+
+olms(
+    'map',
+    'https://api.maptiler.com/maps/topo/style.json?key=Get your own API key at https://www.maptiler.com/cloud/'
+).then($.proxy(function(map) {
+    this.map = map;
+    map.addControl(new ol.control.FullScreen());
+    visualizeBusinesses();
+}, this));
